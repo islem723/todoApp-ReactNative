6319f5bd4b2d239974affb34502a48699a654fc9
islem naffeti <islem.naffeti@esprit.tn>
2023-12-22 18:32:25 +0100

    updated config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import { ATLAS_URL, __DEV__ } from "./env";
 
 export default async function connectDB(connectedCallback: () => void) {
+  const mongoUrl = __DEV__ ? "mongodb://127.0.0.1:27017/todoappDev" : ATLAS_URL;
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/todoapp");
+    const conn = await mongoose.connect(mongoUrl);
     if (conn) {
       console.log(`connected to: ${conn.connection.db.databaseName}`);
       connectedCallback();
--- src/config/env.ts
+++ src/config/env.ts
@@ -5,3 +5,5 @@ config();
 export const JWT_SECRET = `${process.env.JWT_SECRET}`;
 export const JWT_EXPIRATION = `${process.env.JWT_EXPIRATION}`;
 export const HASH_ROUNDS = `${process.env.HASH_ROUNDS}`;
+export const __DEV__ = process.env.NODE_ENV !== "prod";
+export const ATLAS_URL = `${process.env.ATLAS_URL}`;
